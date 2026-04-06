b3729989d9db8600ea89314b764456adad4bebdb
waxir <danishdarman06@gmail.com>
2026-04-07 03:44:45 +0500

    prop is commited

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import { useState } from 'react'
+import { useState } from "react";
 
-import './App.css'
+import "./App.css";
+import Card from "./components/Card";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <h1 className='bg-amber-500 rounded-xl p-4 '>Tailwind Test</h1>
+      <h1 className="bg-amber-500 rounded-xl p-4 ">Tailwind Test</h1>
+      <Card aman = "danish" />
+      <Card aman= "Waxiran"/>
+      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
